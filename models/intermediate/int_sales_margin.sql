b0ab44a1c59cf114d6e26529506306f4b18bbb9c
Rob Farley <farleyr2538@icloud.com>
2024-09-10 15:30:48 +0000

    adding purchase_cost and margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,19 @@
+with purchase_cost_table as (
+    
+    select
+        sales.date_date
+        , sales.revenue as revenue
+        , product.purchase_price
+        , sales.quantity as quantity
+        , round(sales.quantity * product.purchase_price, 2) AS purchase_cost
+    from {{ ref('stg_raw__sales') }} as sales
+    join {{ ref('stg_raw__product') }} as product
+    on product.products_id = sales.products_id
+    limit 15
+
+)
+
+select
+    *
+    , round(revenue - purchase_cost, 2) AS margin
+from purchase_cost_table
