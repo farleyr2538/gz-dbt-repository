f9ad75f97899600aab02f591e55863a67fe4906b
Rob Farley <farleyr2538@icloud.com>
2024-09-11 10:24:25 +0000

    updating operational margin table

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,9 @@
 select
-    margin.orders_id
-    , margin.date_date
+    margin.orders_id as orders_id
+    , margin.date_date as date_date
+    , ship.shipping_fee as ship_fee
+    , ship.ship_cost as ship_cost
+    , ship.log_cost as log_cost
     , ROUND(margin.margin + ship.shipping_fee - ship.ship_cost - ship.log_cost, 2) AS operational_margin
 from {{ ref('int_orders_margin') }} as margin
 join {{ ref('stg_raw__ship') }} as ship
